8fdd69acee156a80ca3d96ae0f55c4036687dd46
朱彤 <1251921644@qq.com>
2020-12-16 11:12:15 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/c_learn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_learn.dir/main.c.o"
+  "c_learn"
+  "c_learn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
